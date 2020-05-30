034440d1f36cafe22f1528296af0588cd7ad9e77
haiyoucuv <qiuxu123456@live.com>
2020-05-30 20:11:41 +0800

    Initial Commit

---
+++ TestOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestOpenGL
+//
+//  Created by 还有醋v on 2020/5/30.
+//  Copyright © 2020 还有醋v. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
